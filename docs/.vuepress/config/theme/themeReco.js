90910773693ff44a819c6a99c6588fa8c702bf27
PhoebeNan <13037650105@163.com>
2019-11-29 10:46:19 +0800

    update

--- docs/.vuepress/config/theme/themeReco.js
+++ docs/.vuepress/config/theme/themeReco.js
@@ -4,19 +4,19 @@ module.exports = {
   blogConfig: {
     category: {
       location: 2, // 在导航栏菜单中所占的位置，默认2
-      text: 'Category' // 默认 “分类”
+      text: '分类' // 默认 “分类”
     },
     tag: {
       location: 3, // 在导航栏菜单中所占的位置，默认3
-      text: 'Tag' // 默认 “标签”
+      text: '标签' // 默认 “标签”
     }
   },
   // 最后更新时间
   lastUpdated: 'Last Updated', // string | boolean
   // 作者
-  author: 'reco_luan',
+  author: '赵亚楠',
   // 备案号
-  record: '京ICP备17067634号-1',
+  record: '京ICP备123456号-1',
   // 项目开始时间
-  startYear: '2017'
+  startYear: '2019'
 }
---
+++ public/assets/js/38.224727cf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{121:function(n,w,o){}}]);
